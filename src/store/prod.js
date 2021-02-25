6ef05f9e113028e25265ba1dd475940a2fc90861
Dhruv WO <dhruv.webosmotic@gmail.com>
2021-02-25 16:18:54 +0530

    updated prod.js for app build

--- src/store/prod.js
+++ src/store/prod.js
@@ -1,16 +1,11 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import {createReactNavigationReduxMiddleware} from 'react-navigation-redux-helpers';
 
-import appReducer from './reducers';
+import reducers from './reducers';
 
-const middleware = createReactNavigationReduxMiddleware((state) => state.nav);
-
-/**
- * Creates the store applying Redux Thunk middleware.
- * @returns {Object} The Redux store.
- */
+const middleware = [thunk];
+// eslint-disable-next-line no-underscore-dangle
 const configureStore = () =>
-  createStore(appReducer, applyMiddleware(thunk, middleware));
+  createStore(reducers, applyMiddleware(...middleware));
 
 export default configureStore;
